f731bec63b30176bb845067d05c3d8551a4fe3b6
Sourav Suvarna <96716586+souravsuvarna@users.noreply.github.com>
2023-11-03 17:53:41 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -9,6 +9,18 @@ struct CountsBySoH {
 
 struct CountsBySoH countBatteriesByHealth(const int* presentCapacities, int nBatteries) {
   struct CountsBySoH counts = {0, 0, 0};
+  
+  for (int i = 0; i < nBatteries; i++) {
+    double soh = (double)presentCapacities[i] / 120.0 * 100.0;
+    if (soh > 80.0 && soh <= 100.0) {
+      counts.healthy++;
+    } else if (soh >= 62.0 && soh <= 80.0) {
+      counts.exchange++;
+    } else {
+      counts.failed++;
+    }
+  }
+  
   return counts;
 }
 
